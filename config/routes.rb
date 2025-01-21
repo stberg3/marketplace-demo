3d2e165c0027334f7ce79f516f46115616c8d30f
Sam Berg <stberg@spscommerce.com>
2025-01-21 12:23:27 -0600

    Add 'api' namespace

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do 
+    # We are going to list our resources here 
+  end 
+
 end
