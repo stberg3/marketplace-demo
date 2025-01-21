67fe3412074ad23634e6dbf1412bf1b1004ff8b3
Sam Berg <stberg@spscommerce.com>
2025-01-21 14:48:16 -0600

    Use fancier syntax for route def

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # We are going to list our resources here
-      resources :users, only: [ :show, :create ]
+      resources :users, only: %i[show create]
     end
   end
 end
