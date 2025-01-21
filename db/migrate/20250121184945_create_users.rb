bf1bbee02603978d1d687739cf405d5944115f42
Sam Berg <stberg@spscommerce.com>
2025-01-21 13:04:37 -0600

    Refined the User schema

--- db/migrate/20250121184945_create_users.rb
+++ db/migrate/20250121184945_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
-
+      t.string :email, null: false
+      t.index :email, unique: true
+      t.string :password_digest, null: false
       t.timestamps
     end
   end
