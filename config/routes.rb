9a321a57dacf2edcc0cf0b7af533689f7fd94c0e
Sam Berg <stberg@spscommerce.com>
2025-01-21 15:21:37 -0600

    Add DELETE endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # We are going to list our resources here
-      resources :users, only: %i[show create update]
+      resources :users, only: %i[show create update destroy]
     end
   end
 end
