b0027df11a6547fe183d038c2c777cfa721275da
Sam <stberg3@gmail.com>
2025-01-26 09:21:07 -0600

    Fix "Brakeman" Warning
    I learned about this from
    the ready-bake [GitHub pipeline][1]
    
    [1]: https://github.com/stberg3/marketplace-demo/actions/runs/12975835262/job/36187378672#step:4:152

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
-  validates_format_of :email, with: /@/
+  validates_format_of :email, with: /\A@\z/
   validates :password_digest, presence: true
   has_secure_password
 end
